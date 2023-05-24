e6377325e903ce926741168f6892e5b1f17545e2
Zendr <520530ol@ukr.net>
2023-05-24 09:20:42 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,11 @@
 import { AppContainer } from './App.styled';
-import { Profile } from 'components/Profile/Profile';
+import { Profile } from 'components/Profile/Profile.jsx';
 import user from 'data/user';
-import { Statistics } from 'components/Statistics/Statistics';
+import { Statistics } from 'components/Statistics/Statistics.jsx';
 import data from 'data/data';
-import { FriendsList } from 'components/FriendList/FriendList';
+import { FriendsList } from 'components/FriendList/FriendList.jsx';
 import friends from 'data/friends';
-import { TransactionHistory } from 'components/TransactionHistory/Transactions';
+import { TransactionHistory } from 'components/TransactionHistory/Transactions.jsx';
 import transactions from 'data/transactions';
 export const App = () => {
   return (
